df375b108f79a919288ac634fdba974fb80665c4
semprini42 <yan.uchitel@gmail.com>
2023-05-30 13:43:59 -0400

    added some text

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'skateboard controller',
         theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-            sliderTheme: SliderThemeData(
-              showValueIndicator: ShowValueIndicator.always,
-            )),
+          useMaterial3: true,
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
         home: MyHomePage(),
       ),
     );
@@ -51,34 +49,28 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-          RotatedBox(
-            quarterTurns: 3,
-            child: Slider(
-              min: 0.0,
-              max: 1023.0,
-              value: _value,
-              label: '${_value.round()}',
-              onChanged: (newValue) {
-                setState(() {
-                  _value = newValue;
-                });
-              },
-              onChangeEnd: (newValue) {
-                setState(() {
-                  _value = 0;
-                });
-              },
+          Expanded(
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: Slider(
+                min: 0.0,
+                max: 1023.0,
+                value: _value,
+                label: '${_value.round()}',
+                onChanged: (newValue) {
+                  setState(() {
+                    _value = newValue;
+                  });
+                },
+                onChangeEnd: (newValue) {
+                  setState(() {
+                    _value = 0;
+                  });
+                },
+              ),
             ),
           ),
-          Text(_value.round().toString()),
+          Text('Value: ${_value.round().toString()}'),
         ],
       ),
     );
