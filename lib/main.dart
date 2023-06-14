f6e6eec67972d73e3c91f91277d3f7d7e992a4e6
Semprini42 <yan.uchitel@gmail.com>
2023-06-14 00:34:27 -0400

    working demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:english_words/english_words.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:skateboard_controller/views/home_page.dart';
+import 'package:skateboard_controller/controllers/bluetooth_controller.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,9 +19,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'skateboard controller',
         theme: ThemeData(
-          useMaterial3: true,
-          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
+            // useMaterial3: true,
+            // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+            ),
         // home: MyHomePage(),
         home: HomePage(),
       ),
@@ -28,21 +30,130 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  BluetoothController controller = BluetoothController();
+}
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+              height: 180,
+              width: double.infinity,
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  "Bluetooth App",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              )),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SpeedController()),
+              ),
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(350, 55),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  )),
+              child: const Text(
+                "Control",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () => MyAppState().controller.scanDevices(),
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(350, 55),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  )),
+              child: const Text(
+                "Scan",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          StreamBuilder<List<ScanResult>>(
+              stream: MyAppState().controller.scanResults,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final data = snapshot.data![index];
+                        return ElevatedButton(
+                          onPressed: () async {
+                            await data.device.connect();
+                          },
+                          child: ListTile(
+                            title: Text(data.device.name),
+                            subtitle: Text(data.device.id.id),
+                            trailing: Text(data.rssi.toString()),
+                          ),
+                        );
+                      });
+                } else {
+                  return const Center(child: Text("No devices found"));
+                }
+              })
+        ],
+      ),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class SpeedController extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SpeedController> createState() => _SpeedController();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SpeedController extends State<SpeedController> {
   double _value = 0;
+
+  void changeValue(_value) async {
+    List<BluetoothDevice> test =
+        await MyAppState().controller.flutterBlue.connectedDevices;
+    BluetoothDevice device = test[0];
+
+    List<BluetoothService> services = await device.discoverServices();
+    services.forEach((service) async {
+      var characteristics = service.characteristics;
+      for (BluetoothCharacteristic c in characteristics) {
+        if (c.properties.write) {
+          await c.write(utf8.encode(_value.toString()));
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,11 +173,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 onChanged: (newValue) {
                   setState(() {
                     _value = newValue;
+                    changeValue(_value.toInt());
                   });
                 },
                 onChangeEnd: (newValue) {
                   setState(() {
                     _value = 0;
+                    changeValue(_value.toInt());
                   });
                 },
               ),
@@ -74,7 +187,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Row(
             children: [
-              Text('Value: ${_value.round().toString()}',),
+              Text(
+                'Value: ${_value.round().toString()}',
+              ),
             ],
           ),
         ],
